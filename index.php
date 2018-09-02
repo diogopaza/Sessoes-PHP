e046a42b3d26e6ee0fb02360da1174a06a66cb39
Diogo Paza <diogopazacvel@gmail.com>
2018-09-02 17:01:56 -0300

    testes

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+	
+	echo"
+		<form method='post' action='inicio.php' id='formLogin' name='formLogin'>
+			<fieldset id='field'>
+				<legend>Login</legend>
+					<label>Nome:</label>
+					<input type='text' name='login' id='login'><br />
+					<label>Senha:</label>
+					<input type='password' name='senha' id='senha'><br>
+					<input type='submit' value='Logar'>
+			</fieldset>
+
+
+		</form>
+	";
+
+
+?>
